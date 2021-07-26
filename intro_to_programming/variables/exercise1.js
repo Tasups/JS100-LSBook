c04b73edb51b3f5ca3e4df5a3c59296a47e7ba31
Jason Whisnant <tasups@gmail.com>
2021-07-26 19:59:51 +0000

    Exercise 1 done

---
+++ intro_to_programming/variables/exercise1.js
@@ -0,0 +1,11 @@
+const name = "Viktor";
+
+const greetMorning = `Good morning ${name}!`;
+const greetNoon = `Hope you're having a wonderful afternoon, ${name}.`;
+const greetEvening = `Hey, ${name}, it sure is cool outside, no?`;
+
+console.log(greetMorning);
+console.log(greetNoon);
+console.log(greetEvening);
+
+
