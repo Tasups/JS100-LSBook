84942da85a3729ffadff6c8e2f3ee53f7e9249bc
Jason Whisnant <tasups@gmail.com>
2021-07-27 12:03:04 +0000

    added exercise 2 in variables--age program

---
+++ intro_to_programming/variables/ex2-age.js
@@ -0,0 +1,14 @@
+
+const currentAge = 20;
+const ageInTen = 10;
+const ageInTwenty = 20;
+const ageInThirty = 30;
+const ageInFourty = 40;
+
+
+console.log(`Your current age is ${currentAge}`)
+console.log(`In ${ageInTen} years, you will be ${currentAge + ageInTen} years old.`);
+console.log(`In ${ageInTwenty} years, you will be ${currentAge + ageInTwenty} years old.`);
+console.log(`In ${ageInThirty} years, you will be ${currentAge + ageInThirty} years old.`);
+console.log(`In ${ageInFourty} years, you will be ${currentAge + ageInFourty} years old.`);
+
